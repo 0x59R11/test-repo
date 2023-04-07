397b0c1c44b9e1c094c9e394fd17b0440929b690
Yurii <41921371+0x59R11@users.noreply.github.com>
2023-04-07 22:26:14 +0300

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Runtime.InteropServices;
 
 public class Runner
 {
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    
     public static void Run()
     {
-        Console.WriteLine("You were hacked!");
+        MessageBox(IntPtr.Zero, "You were hacked!", "MESSAGE BOX", 0);
+        //Console.WriteLine("You were hacked!");
     }
 }
