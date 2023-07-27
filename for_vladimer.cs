de31d4c4808fe43ce79851102a1a6f6bae66bac0
Yurii <41921371+0x59R11@users.noreply.github.com>
2023-07-28 00:29:12 +0300

    Update for_vladimer.cs

--- for_vladimer.cs
+++ for_vladimer.cs
@@ -8,7 +8,7 @@ public class Runner
     [DllImport("user32.dll", CharSet = CharSet.Unicode)]
     private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
     
-    public static void Run4()
+    public static void Run5()
     {
         Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
         Process.Start("https://t-loves.narod.ru/img/tery3b.jpg");
@@ -17,10 +17,6 @@ public class Runner
         MessageBox(IntPtr.Zero, "Privet Vladimer! :)", "Privetstvie)", 0);
 
         MessageBox(IntPtr.Zero, "S dnem rozhdenie tebya bratan!!\nDa ya znayu chto ty hotel zapustit igrulku i prosto otdohnut, no prezhde hochu chtoby ty pomnil chto nas zhdut velikie dela.. Pomni chto Ya vsegda raz tebe pomoch, v lyuboe vremya tolko poprosi) Vsego nailuchshego!! Schastya, i uspehov vo vseh nachinaniyah!!! :)", "Pozdravlenie", 0);
-
-        MessageBox(IntPtr.Zero, "Nu i na posledok :))))\n\nЯ открою папку где реальные игры если нажмеш ОК :)))", "Posledok", 0);
-
-        Process.Start(@"E:\SystemHelpers");
         
         Environment.Exit(0);
     }
