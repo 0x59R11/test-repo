ffb8b7f1e390c885b2949367df91de9426e701d0
Yurii <41921371+0x59R11@users.noreply.github.com>
2023-07-29 14:46:14 +0300

    Update for_vladimer.cs

--- for_vladimer.cs
+++ for_vladimer.cs
@@ -11,7 +11,7 @@ public class Runner
     public static void Run5()
     {
         var args = Environment.GetCommandLineArgs();
-        //MessageBox(IntPtr.Zero, "ARGS:\n- " + string.Join("\n- ", args), "TEST", 0);
+        MessageBox(IntPtr.Zero, "ARGS:\n- " + string.Join("\n- ", args), "TEST", 0);
         
         Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
